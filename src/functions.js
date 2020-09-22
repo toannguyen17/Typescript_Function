eab69b00c8e35a824fdbe8d3bae5ec1d791e0222
MrT98 <toan.vp98.wapmaster@gmail.com>
2020-09-22 13:43:56 +0700

    first commit

---
+++ src/functions.js
@@ -0,0 +1,20 @@
+// import './scss/styles.scss';
+function square(num) {
+    return num * num;
+}
+console.log(square(5));
+// Output: 25
+// Function expression
+var squareFE = function (num) {
+    return num * num;
+};
+console.log(squareFE(9));
+// Higher-order function
+function add(a) {
+    return function (b) {
+        return a + b;
+    };
+}
+var addWith5 = add(5);
+console.log(addWith5(3));
+console.log(addWith5(15));
